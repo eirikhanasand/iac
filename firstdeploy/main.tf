ad707d70ae462051cefe7f1f9db9ba84ef0f98ca
eirikhanasand <eirik.hanasand@gmail.com>
2024-09-23 14:07:12 +0200

    Initial commit

---
+++ firstdeploy/main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "fd-rg" {
+    name     = "rg-demo-we"
+    location = "West Europe"
+}
+
+resource "azurerm_storage_account" "sa-demo" {
+    name                     = "eirikdemo12345"
+    resource_group_name      = azurerm_resource_group.fd-rg.name
+    location                 = azurerm_resource_group.fd-rg.location
+    account_tier             = "Standard"
+    account_replication_type = "GRS" 
+}
